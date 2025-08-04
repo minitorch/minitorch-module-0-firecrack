1367806f5a4b12793a75fec39262204c7d39b42a
shangchi <shangchixmu@163.com>
2025-08-04 10:28:48 +0800

    finish work1

--- minitorch/datasets.py
+++ minitorch/datasets.py
@@ -4,7 +4,17 @@ from dataclasses import dataclass
 from typing import List, Tuple
 
 
-def make_pts(N):
+def make_pts(N: int) -> List[Tuple[float, float]]:
+    """Generate N random points
+
+    Arags:
+      N: Number of points to generate
+
+    Returns
+    -------
+      A list of N point
+
+    """
     X = []
     for i in range(N):
         x_1 = random.random()
@@ -20,7 +30,17 @@ class Graph:
     y: List[int]
 
 
-def simple(N):
+def simple(N: int) -> Graph:
+    """生成一个简单的二分类数据集图形对象。
+    该函数根据给定的点数N, 创建一个基于x_1坐标值的二分类数据集。
+    当x_1坐标小于0.5时, 分类标签为1; 否则为0。
+
+    参数:
+        N (int): 要生成的数据点数量
+
+    返回:
+        Graph: 包含N个数据点、坐标集合X和对应标签y的图形对象
+    """
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -29,7 +49,16 @@ def simple(N):
     return Graph(N, X, y)
 
 
-def diag(N):
+def diag(N: int) -> Graph:
+    """生成分割线为对角线的二维数据集
+
+    参数:
+        N (int): 要生成的数据点数量
+
+    返回:
+        Graph: 包含N个数据点、坐标集合X和对应标签y的图形对象
+
+    """
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -38,7 +67,16 @@ def diag(N):
     return Graph(N, X, y)
 
 
-def split(N):
+def split(N: int) -> Graph:
+    """生成一个具有非线性标签边界的二维分类数据集。
+
+    参数:
+        N (int): 要生成的数据点数量
+
+    返回:
+        Graph: 包含N个数据点、坐标集合X和对应标签y的图形对象
+
+    """
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -47,7 +85,16 @@ def split(N):
     return Graph(N, X, y)
 
 
-def xor(N):
+def xor(N: int) -> Graph:
+    """生成一个具有XOR边界的二维分类数据集。
+
+    参数:
+        N (int): 要生成的数据点数量
+
+    返回:
+        Graph: 包含N个数据点、坐标集合X和对应标签y的图形对象
+
+    """
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -56,7 +103,16 @@ def xor(N):
     return Graph(N, X, y)
 
 
-def circle(N):
+def circle(N: int) -> Graph:
+    """生成一个具有圆形边界的二维分类数据集。
+
+    参数:
+        N (int): 要生成的数据点数量
+
+    返回:
+        Graph: 包含N个数据点、坐标集合X和对应标签y的图形对象
+
+    """
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -66,20 +122,40 @@ def circle(N):
     return Graph(N, X, y)
 
 
-def spiral(N):
+def spiral(N: int) -> Graph:
+    """生成一个具有螺旋的二维分类数据集。
 
-    def x(t):
+    参数:
+        N (int): 要生成的数据点数量
+
+    返回:
+        Graph: 包含N个数据点、坐标集合X和对应标签y的图形对象
+
+    """
+
+    def x(t: float) -> float:
         return t * math.cos(t) / 20.0
 
-    def y(t):
+    def y(t: float) -> float:
         return t * math.sin(t) / 20.0
-    X = [(x(10.0 * (float(i) / (N // 2))) + 0.5, y(10.0 * (float(i) / (N //
-        2))) + 0.5) for i in range(5 + 0, 5 + N // 2)]
-    X = X + [(y(-10.0 * (float(i) / (N // 2))) + 0.5, x(-10.0 * (float(i) /
-        (N // 2))) + 0.5) for i in range(5 + 0, 5 + N // 2)]
+
+    X = [
+        (x(10.0 * (float(i) / (N // 2))) + 0.5, y(10.0 * (float(i) / (N // 2))) + 0.5)
+        for i in range(5 + 0, 5 + N // 2)
+    ]
+    X = X + [
+        (y(-10.0 * (float(i) / (N // 2))) + 0.5, x(-10.0 * (float(i) / (N // 2))) + 0.5)
+        for i in range(5 + 0, 5 + N // 2)
+    ]
     y2 = [0] * (N // 2) + [1] * (N // 2)
     return Graph(N, X, y2)
 
 
-datasets = {'Simple': simple, 'Diag': diag, 'Split': split, 'Xor': xor,
-    'Circle': circle, 'Spiral': spiral}
+datasets = {
+    "Simple": simple,
+    "Diag": diag,
+    "Split": split,
+    "Xor": xor,
+    "Circle": circle,
+    "Spiral": spiral,
+}
--- minitorch/operators.py
+++ minitorch/operators.py
@@ -33,6 +33,170 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.1.
+def mul(x: float, y: float) -> float:
+    """Multiply two numbers"""
+    return x * y
+
+
+def id(x: float) -> float:
+    """Return the input"""
+    return x
+
+
+def add(x: float, y: float) -> float:
+    """Add two numbers"""
+    return x + y
+
+
+def neg(x: float) -> float:
+    """Return the negative of a number"""
+    return -x
+
+
+def lt(x: float, y: float) -> float:
+    """Return 1.0 if x is less than y and 0.0 otherwise"""
+    return 1.0 if x < y else 0.0
+
+
+def eq(x: float, y: float) -> float:
+    """Return 1.0 if x is equal to y and 0.0 otherwise"""
+    return 1.0 if x == y else 0.0
+
+
+def max(x: float, y: float) -> float:
+    """Return the maximum of two numbers"""
+    return x if x > y else y
+
+
+def is_close(x: float, y: float) -> float:
+    """Return 1.0 if x and y are less than 1e-2 apart, and 0.0 otherwise."""
+    return 1.0 if abs(x - y) < 1e-2 else 0.0
+
+
+def sigmoid(x: float) -> float:
+    """Compute the sigmoid function of x
+
+    Args:
+    ----
+        x: A scalar or numpy array.
+
+    Returns:
+    -------
+        1 / (1 + exp(-x))
+
+    """
+    return 1 / (1 + math.exp(-x))
+
+
+def relu(x: float) -> float:
+    """Compute the relu function of x
+
+    Args:
+    ----
+        x: A scalar or numpy array.
+
+    Returns:
+    -------
+        x if x is greater than or equal to 0, 0 otherwise
+
+    """
+    return x if x >= 0 else 0
+
+
+def log(x: float) -> float:
+    """Compute the natural logarithm of x
+
+    Args:
+    ----
+        x: A scalar or numpy array.
+
+    Returns:
+    -------
+        log(x)
+
+    """
+    return math.log(x)
+
+
+def exp(x: float) -> float:
+    """Compute the exp function of x
+
+    Args:
+    ----
+        x: A scalar or numpy array.
+
+    Returns:
+    -------
+        e^x
+
+    """
+    return math.exp(x)
+
+
+def inv(x: float) -> float:
+    """Compute the inverse function of x
+
+    Args:
+    ----
+        x: A scalar or numpy array.
+
+    Returns:
+    -------
+        1/x
+
+    """
+    return 1.0 / x
+
+
+def log_back(x: float, y: float) -> float:
+    """Compute the gradient function for log
+        ∂L/∂x = ∂L/∂ln(x) * (1/x) = y / x
+
+    Args:
+    ----
+        x: A scalar or numpy array.
+        y: A scalar, ∂L/∂ln(x)
+
+    Returns:
+    -------
+        y / x
+
+    """
+    return y / x
+
+
+def inv_back(x: float, y: float) -> float:
+    """Compute the gradient function for inv
+        ∂L/∂x = ∂L/∂(1/x) * (-x^-2) = -y / x^2
+
+    Args:
+    ----
+        x: A scalar or numpy array.
+        y: A scalar, ∂L/∂(1/x)
+
+    Returns:
+    -------
+        -y / x^2
+
+    """
+    return -y / x**2
+
+
+def relu_back(x: float, y: float) -> float:
+    """Compute the gradient function for relu
+        ∂L/∂x = ∂L/∂(ReLU(x)) * (0 if x < 0 else 1) = y * (0 if x < 0 else 1)
+
+    Args:
+    ----
+        x: A scalar or numpy array.
+        y: A scalar, ∂L/∂(ReLU(x))
+
+    Returns:
+    -------
+        y if x >= 0 else 0
+
+    """
+    return y * (x >= 0)
 
 
 # ## Task 0.3
@@ -52,3 +216,66 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.3.
+def map(a: Iterable[float], fn: Callable) -> Iterable[float]:
+    """Map a function onto a list"""
+    return [fn(x) for x in a]
+
+
+def zipWith(
+    fn: Callable[[float, float], float], a: Iterable[float], b: Iterable[float]
+) -> Iterable[float]:
+    """Combines two iterables element-wise using a specified function
+    Args:
+        fn: function to combine elements
+        a: first iterable of floats, eg. list
+        b: second iterable of floats, eg. list
+
+    Returns
+    -------
+        A list of combined elements
+
+    """
+    return [fn(x, y) for x, y in zip(a, b)]
+
+
+def reduce(
+    a: Iterable[float], fn: Callable[[float, float], float], start: float = 0.0
+) -> float:
+    """Applies a function of two arguments cumulatively to the items of an
+    iterable, so as to reduce the iterable to a single float value.
+
+    Args:
+    ----
+        fn: function to apply
+        a: iterable of numbers
+        start: starting value
+
+    Returns:
+    -------
+        A single number
+
+    """
+    res = start
+    for i in a:
+        res = fn(res, i)
+    return res
+
+
+def negList(ls: Iterable[float]) -> Iterable[float]:
+    """Negate all elements"""
+    return map(ls, neg)
+
+
+def addLists(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
+    """Add two lists"""
+    return zipWith(add, ls1, ls2)
+
+
+def sum(ls: Iterable[float]) -> float:
+    """Sum up a list"""
+    return reduce(ls, add, 0.0)
+
+
+def prod(ls: Iterable[float]) -> float:
+    """Multiply all elements"""
+    return reduce(ls, mul, 1.0)
